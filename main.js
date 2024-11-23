4be9bae3afdfccc037ae4b3bf16a6c62e4385bda
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-23 11:54:46 -0300

    Configurando o CREATE do CRUD

--- main.js
+++ main.js
@@ -5,7 +5,26 @@ const openModal = () =>
 const closeModal = () =>
   document.getElementById("modal").classList.remove("active");
 
+const tempClient = {
+  name: "Daniel",
+  email: "daniel@.com",
+  celular: "11999999999",
+  cidade: "Recife",
+};
+
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("db_client")) ?? [];
+const setLocalStorage = (dbClient) =>
+  localStorage.setItem("db_client", JSON.stringify(dbClient));
+
 //CRUD - Create, Read, Update, Delete
+
+//CRUD CREATE
+const createClient = (client) => {
+  const dbClient = getLocalStorage();
+  dbClient.push(client);
+  setLocalStorage(dbClient);
+};
 
 //Evento
 document
