0c4e16b765fef16c0b443daeb9772d9df0b56640
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-25 19:40:57 -0300

    Configurando funçoes do CRUD

--- main.js
+++ main.js
@@ -2,14 +2,9 @@
 
 const openModal = () =>
   document.getElementById("modal").classList.add("active");
-const closeModal = () =>
+const closeModal = () => {
+  clearFields();
   document.getElementById("modal").classList.remove("active");
-
-const tempClient = {
-  name: "Daniel",
-  email: "daniel@.com",
-  celular: "11999999999",
-  cidade: "Recife",
 };
 
 const getLocalStorage = () =>
@@ -43,14 +38,47 @@ const createClient = (client) => {
   setLocalStorage(dbClient);
 };
 
-const isValidFields = () => {};
+const isValidFields = () => {
+  return document.getElementById("form").reportValidity();
+};
 
 //Interação com o layout
+
+const clearFields = () => {
+  const fields = document.querySelectorAll(".modal-field");
+  fields.forEach((field) => (field.value = ""));
+};
 const salveClient = () => {
   if (isValidFields()) {
-    console.log("Cadastrando cliente");
+    const client = {
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      celular: document.getElementById("celular").value,
+      cidade: document.getElementById("cidade").value,
+    };
+    createClient(client);
+    closeModal();
   }
 };
+
+const createRow = (client) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+            <td>${client.nome}</td>
+            <td>${client.email}</td>
+            <td>${client.celular}</td>
+            <td>${client.cidade}</td>
+            <td>
+              <button type="button" class="button green">Editar</button>
+              <button type="button" class="button red">Excluir</button>`;
+};
+
+const updateTable = () => {
+  const dbClient = readClient();
+  dbClient.forEach(createRow);
+};
+
+updateTable();
 
 //Evento
 document
