7e3393ae3ded1a5d0320aba86c8334285cdfa615
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-26 21:10:45 -0300

    Responsividade CSS no CRUD

--- main.js
+++ main.js
@@ -47,8 +47,10 @@ const isValidFields = () => {
 const clearFields = () => {
   const fields = document.querySelectorAll(".modal-field");
   fields.forEach((field) => (field.value = ""));
+  document.getElementById("nome").dataset.index = "new";
+  document.querySelector(".modal-header h2").textContent = "Novo Cliente";
 };
-const salveClient = () => {
+const saveClient = () => {
   if (isValidFields()) {
     const client = {
       nome: document.getElementById("nome").value,
@@ -56,27 +58,87 @@ const salveClient = () => {
       celular: document.getElementById("celular").value,
       cidade: document.getElementById("cidade").value,
     };
-    createClient(client);
-    closeModal();
+    const index = document.getElementById("nome").dataset.index;
+    if (index === "new") {
+      createClient(client);
+      updateTable();
+      closeModal();
+    } else {
+      updateClient(index, client);
+      updateTable();
+      closeModal();
+    }
   }
 };
 
-const createRow = (client) => {
+const createRow = (client, index) => {
   const newRow = document.createElement("tr");
   newRow.innerHTML = `
-            <td>${client.nome}</td>
-            <td>${client.email}</td>
-            <td>${client.celular}</td>
-            <td>${client.cidade}</td>
-            <td>
-              <button type="button" class="button green">Editar</button>
-              <button type="button" class="button red">Excluir</button>`;
+    <td>${client.nome}</td>
+    <td>${client.email}</td>
+    <td>${client.celular}</td>
+    <td>${client.cidade}</td>
+    <td>
+      <button type="button" class="button green" id="edit-${index}">Editar</button>
+      <button type="button" class="button red" id="excluir-${index}">Excluir</button>
+    </td>`;
+
+  document.querySelector("#tableClient > tbody").appendChild(newRow);
+};
+
+const clearTable = () => {
+  const rows = document.querySelectorAll("#tableClient > tbody tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
 };
 
 const updateTable = () => {
   const dbClient = readClient();
+  clearTable();
   dbClient.forEach(createRow);
 };
+
+const fillFields = (client) => {
+  document.getElementById("nome").value = client.nome;
+  document.getElementById("email").value = client.email;
+  document.getElementById("celular").value = client.celular;
+  document.getElementById("cidade").value = client.cidade;
+  document.getElementById("nome").dataset.index = client.index;
+};
+
+const editCliente = (index) => {
+  const client = readClient()[index];
+  client.index = index;
+  fillFields(client);
+  document.querySelector(
+    ".modal-header > h2"
+  ).textContent = `Editando ${client.nome}`;
+  openModal();
+};
+
+const editDelete = (event) => {
+  if (event.target.type === "button") {
+    const [action, index] = event.target.id.split("-");
+    if (action === "edit") {
+      editCliente(index);
+    } else {
+      const client = readClient()[index];
+      const response = confirm(
+        `Deseja realmente excluir o cliente ${client.nome}?`
+      );
+      if (response) {
+        deleteClient(index);
+        updateTable();
+      }
+    }
+  }
+};
+
+document.getElementById("form").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Impede o comportamento padrão do formulário
+    document.getElementById("salvar").click(); // Aciona o clique no botão "Salvar"
+  }
+});
 
 updateTable();
 
@@ -86,4 +148,10 @@ document
   .addEventListener("click", openModal);
 document.getElementById("modalClose").addEventListener("click", closeModal);
 
-document.getElementById("salvar").addEventListener("clickk", salveClient);
+document.getElementById("salvar").addEventListener("click", saveClient);
+
+document
+  .querySelector("#tableClient > tbody")
+  .addEventListener("click", editDelete);
+
+document.getElementById("cancelar").addEventListener("click", closeModal);
